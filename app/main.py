2a9eb723a60cfc8efc36567fd5e32ea7d386f5db
Yaroslav <didkivskiyyaroslav@gmail.com>
2026-03-12 02:15:47 +0200

    feat: add router

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI
 
 from app.config import get_settings
-
+from app.routers import sessions_router
 
 settings = get_settings()
 
 app = FastAPI(
     title=settings.app_name,
     debug=settings.debug,
-    version="0.1.0"
+    version="0.1.0",
 )
+
+app.include_router(sessions_router)
 
 
 @app.get("/health")
