02379653bb82d88cbf6bf16300da74782a87e967
Yaroslav <didkivskiyyaroslav@gmail.com>
2026-03-12 03:33:31 +0200

    feat: add answer router

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,3 +1,5 @@
 from app.routers.sessions import router as sessions_router
+from app.routers.answers import router as answers_router
+from app.routers.health import router as health_router
 
-__all__ = ["sessions_router"]
+__all__ = ["sessions_router", "answers_router", "health_router"]
