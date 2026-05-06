bf0d6d5ae988a829fa9fedf4f71295e3600db852
Yaroslav <didkivskiyyaroslav@gmail.com>
2026-05-07 01:41:31 +0300

    chore: add question bank to init list

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,10 +2,12 @@ from app.models.answer import Answer
 from app.models.feedback import Feedback
 from app.models.question import Question
 from app.models.session import InterviewSession
+from app.models.question_bank import QuestionBank
 
 __all__ = [
     "InterviewSession",
     "Question",
     "Answer",
     "Feedback",
+    "QuestionBank",
 ]
