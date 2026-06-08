eac3fca51ed51b0a7fd91237d0cac7cd828dfa2e
Yaroslav <didkivskiyyaroslav@gmail.com>
2026-06-08 23:21:58 +0300

    hotfix: fix celery beat sh file

--- beat_entrypoint.sh
+++ beat_entrypoint.sh
@@ -1,23 +1,7 @@
 #!/bin/sh
 set -e
 
-python -c "
-import threading, http.server, os
-
-class Handler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'ok')
-    def log_message(self, *args):
-        pass
-
-def serve():
-    port = int(os.environ.get('PORT', 8080))
-    http.server.HTTPServer(('0.0.0.0', port), Handler).serve_forever()
-
-threading.Thread(target=serve, daemon=True).start()
-" &
+python -m http.server ${PORT:-8080} &
 
 echo "Starting Celery beat..."
 exec celery -A app.celery_app.celery_app beat --loglevel=info
