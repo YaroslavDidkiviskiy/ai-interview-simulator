cbc1411dca40386e2ad300f6b586ec67e376f5ce
Yaroslav <didkivskiyyaroslav@gmail.com>
2026-06-08 23:16:24 +0300

    hotfix: fix sh script

--- worker_entrypoint.sh
+++ worker_entrypoint.sh
@@ -1,23 +1,9 @@
 #!/bin/sh
 set -e
 
-python -c "
-import threading, http.server, os
+python -m http.server ${PORT:-8080} &
+HTTP_PID=$!
 
-class Handler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'ok')
-    def log_message(self, *args):
-        pass
+celery -A app.celery_app.celery_app worker --loglevel=info
 
-def serve():
-    port = int(os.environ.get('PORT', 8080))
-    http.server.HTTPServer(('0.0.0.0', port), Handler).serve_forever()
-
-threading.Thread(target=serve, daemon=True).start()
-" &
-
-echo "Starting Celery worker..."
-exec celery -A app.celery_app.celery_app worker --loglevel=info
+kill $HTTP_PID
